e0428517b7ac649b8986004c5f0a60cc006f4df1
VladMiryaha <vlad.miryaha@gmail.com>
2015-09-21 23:33:56 +0300

    ok

--- to 15.09.cpp
+++ to 15.09.cpp
@@ -2,20 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
+const int MaxTokensCount = 100;
+
 void Split(char* astring, char* delimiters, char*** tokens, int* tokensCount)
 {
-    char * wordPointer;
+    char* word;
     int i = 0;
-    wordPointer = strtok(astring, delimiters);
-  /*
-   * FIXIT: форматирование: пробелы вокруг бинарных операций.
-   * —тарайтесь называть переменные более осмысленно: если вместо char* p; можно написать char* token;
-   */
-    while (wordPointer != NULL)
+    word = strtok(astring, delimiters);
+
+    while (word != NULL)
     {
-        (*tokens)[i] = wordPointer;
+        (*tokens)[i] = word;
         i++;
-        wordPointer = strtok(NULL, delimiters);
+        word = strtok(NULL, delimiters);
     }
     *tokensCount = i;
 }
@@ -25,26 +24,14 @@ int main()
     char s[] = "asdasd     asdahsd         asduyiasd           dfghfgh          jhhjjh";
     char delim[] = " ";
     int tokensCount;
-  /*
-   * «ачем это кручение-верчение указателей: уберите все лишние переменные.
-   * int tokensCount;
-   * char** tokens;
-   * tokens = (char**)malloc...
-   * Split(s, delims, &tokens, &tokensCount);
-   */
-    char ** tokens;
-  /*
-   * 1. ” вас в некоторых местах отступы сделаны пробелами, а где-то символами табул€ции.
-   * ” мен€ форматирование поехало из-за этого.
-   * 2. ѕам€ть надо выдел€ть в том же месте, где вы еЄ освобождаете. “.к. внутри ф-и split вы еЄ освободить не можете, то и выделить еЄ надо в main`е.
-   * 3. tokk - плохое, не внос€щие никакой €сности название. будто пон€тные слова кончились.
-   * 4. надо освободить пам€ть.
-   */
-    tokens = (char**)malloc(100 * sizeof(char*));
+    char** tokens;
+ 
+    tokens = (char**)malloc(MaxTokensCount * sizeof(char*));
     Split(s, delim, &tokens, &tokensCount);
     for (int i = 0; i < tokensCount; i++)
         printf("%s\n", tokens[i]);
     printf("String contains %d word(s).", tokensCount);
+
     free(tokens);
     return 0;
 }
