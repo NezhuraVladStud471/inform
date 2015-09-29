e8293a08dbc2ef8c4c608f5a93e33f0ba00d1cd9
NezhuraVladStud471 <vlad.nezhura@mail.ru>
2015-09-29 13:31:32 +0300

    first task to 29.09

---
+++ 2b_to_29.09.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main()
+{
+    int status, n, i = 0;
+    scanf("%d", &n);
+    while (i < n)
+    {
+        pid_t pid = fork();
+        i++;
+        if ((i == n) && (pid == 0))         //last child mustn't print 'status'
+            exit(0);
+        if (pid > 0)
+            break;
+    }
+    wait(&status);
+    printf("\n%d", status);
+    return 0;
+}
